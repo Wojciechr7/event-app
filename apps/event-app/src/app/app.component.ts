5582f224d10608f23a4d5a565bda605b3fe39941
Fsafasf <asfasf>
2020-06-06 14:18:44 +0200

    initial commit

---
+++ apps/event-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@event-app/api-interfaces';
+
+@Component({
+  selector: 'event-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}
