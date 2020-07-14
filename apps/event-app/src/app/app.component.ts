4b07efd5ee63e21dd13f32714a94d4773f8a7ab7
Fsafasf <asfasf>
2020-07-14 21:10:35 +0200

    add firebase cloud messaging config

--- apps/event-app/src/app/app.component.ts
+++ apps/event-app/src/app/app.component.ts
@@ -1,14 +1,72 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@event-app/api-interfaces';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireMessaging } from "@angular/fire/messaging";
+import { CLOUD_MESSAGING_TOKEN } from "../firebase-config";
 
 @Component({
   selector: 'event-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {
+export class AppComponent implements OnInit {
+
+  token: string;
+
+  constructor(
+    private angularFireMessaging: AngularFireMessaging
+  ) {
   }
+
+  ngOnInit() {
+    this.requestPermission()
+  }
+
+  test() {
+    console.log(this.token)
+  }
+
+  subToTopic() {
+    this.subscribeTokenToTopic(this.token, 'all');
+  }
+
+  updateApp() {
+    window.location.reload();
+  }
+
+  requestPermission() {
+    this.angularFireMessaging.requestToken.subscribe(
+      (token) => {
+        this.token = token;
+        this.subscribeTokenToTopic(token, 'all');
+        console.log(token);
+      },
+      (err) => {
+        this.token = err;
+        console.error('Unable to get permission to notify.', err);
+      }
+    );
+  }
+
+  receiveMessage() {
+    this.angularFireMessaging.messages.subscribe(
+      (payload) => {
+        console.log("new message received. ", payload);
+      })
+  }
+
+  private subscribeTokenToTopic(token, topic) {
+    fetch('https://iid.googleapis.com/iid/v1/' + token + '/rel/topics/' + topic, {
+      method: 'POST',
+      headers: new Headers({
+        'Authorization': `key=${ CLOUD_MESSAGING_TOKEN }`
+      })
+    }).then(response => {
+      if (response.status < 200 || response.status >= 400) {
+        throw 'Error subscribing to topic: ' + response.status + ' - ' + response.text();
+      }
+      console.log('Subscribed to "' + topic + '"');
+    }).catch(error => {
+      console.error(error);
+    })
+  }
+
 }
--- apps/event-app/src/app/app.module.ts
+++ apps/event-app/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFireMessagingModule } from "@angular/fire/messaging";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireMessagingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
