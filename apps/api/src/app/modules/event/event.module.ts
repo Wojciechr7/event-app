2d9a9a448db38470e4618c2b7a2b60529f0ca2a8
Fsafasf <asfasf>
2020-07-25 10:41:57 +0200

    prepare to deploy api

--- apps/api/src/app/modules/event/event.module.ts
+++ apps/api/src/app/modules/event/event.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { EventController } from "./controllers/event/event.controller";
 import { EventService } from "./services/event/event.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { EventSchema } from "./schemas/event.schema";
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Event', schema: EventSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Event', schema: EventSchema }]),
+    HttpModule
+  ],
   controllers: [EventController],
   providers: [EventService]
 })
--- apps/api/src/app/modules/event/services/event/event.service.ts
+++ apps/api/src/app/modules/event/services/event/event.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
 import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 import { EventModel } from "../../models/event.model";
 import { CreateEventDTO } from "../../../../../../../../libs/api-interfaces/src/lib/dto/create-event.dto";
+import { catchError } from "rxjs/operators";
 
 @Injectable()
 export class EventService {
-  constructor(@InjectModel('Event') private readonly eventModel: Model<EventModel>) {
+  constructor(
+    @InjectModel('Event') private readonly eventModel: Model<EventModel>,
+    private httpService: HttpService
+  ) {
   }
 
   getAllEvents() {
@@ -15,6 +19,32 @@ export class EventService {
 
   async addEvent(createEventDTO: CreateEventDTO): Promise<EventModel> {
     const newEvent = await new this.eventModel(createEventDTO);
+
+
+    this.httpService.post('https://fcm.googleapis.com/fcm/send',
+      {
+        "notification": {
+          "title": "cześć",
+          "body": "tu robcio :) v2"
+        },
+        "to": "/topics/all"
+      },
+      {
+        headers: {
+          "Authorization": "key=AAAAid4goBo:APA91bHTYuLboDJ-fMDVvPoIJXmbQKoNVk_71kX62XGov3iYIyw6AvcXyMczeGS2GxKOINPfFDTyb98EQ-9h_Qf3HqV3if3bfHz4qn7XwT9vBAp8iu_epHWP8uQRoOs28H_mmBOx3mJq",
+          "Content-Type": "application/json"
+        }
+      }
+      ).pipe(
+        catchError(e => {
+          console.log(e)
+          return e;
+        })
+    )
+      .subscribe(v => {
+      console.log(v)
+    })
+
     return newEvent.save();
   }
 
