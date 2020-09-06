02e2d9c68816586a5cf50415b85e76dbe2bb056d
Fsafasf <asfasf>
2020-09-06 13:32:37 +0200

    add app state

--- apps/event-management/src/app/app.module.ts
+++ apps/event-management/src/app/app.module.ts
@@ -3,12 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import * as fromApp from './+state/app.reducer';
+import { AppEffects } from './+state/app.effects';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot(),
+    StoreModule.forFeature(fromApp.APP_FEATURE_KEY, fromApp.reducer),
+    EffectsModule.forFeature([AppEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent],
